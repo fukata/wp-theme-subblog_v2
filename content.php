66f80d615a9fb03f51b9e19efc2e196926b1c212
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2015-01-25 01:59:37 +0700

    change excerpt sentence color

--- content.php
+++ content.php
@@ -65,7 +65,9 @@
                 <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
 			</h1>
 
-            <?php the_excerpt(); ?>
+            <div class="entry-excerpt">
+                <?php the_excerpt(); ?>
+            </div>
 
 			<?php twentytwelve_entry_meta(); ?>
 			<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
