d1908be1e872caf070922f87b6d05c25ecb85ee1
fukata <tatsuya.fukata@gmail.com>
2016-05-01 22:12:40 +0700

    add ad title

--- content_bottom_ad.php
+++ content_bottom_ad.php
@@ -1,11 +1,12 @@
             <?php if ( is_single() ) : ?>
 <div class="ad_section">
-    <p class="ad_title">広告</p>
     <div class="clear">
         <div class="ad">
+            <p class="ad_title">広告</p>
             <?php echo stripslashes(get_option('my_ad_content_bottom1')); ?>
         </div>
         <div class="ad">
+            <p class="ad_title">広告</p>
             <?php echo stripslashes(get_option('my_ad_content_bottom2')); ?>
         </div>
     </div>
