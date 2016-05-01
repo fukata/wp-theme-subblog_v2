7f779d37e483b197eebc10ab4c2da176fba4a2e5
fukata <tatsuya.fukata@gmail.com>
2016-05-01 21:00:04 +0700

    update ad

--- content_bottom_ad.php
+++ content_bottom_ad.php
@@ -1,11 +1,13 @@
             <?php if ( is_single() ) : ?>
-<p>スポンサードリンク</p>
-<div class="clear">
-    <div class="ad">
-        <?php echo stripslashes(get_option('my_ad_content_bottom1')); ?>
-    </div>
-    <div class="ad">
-        <?php echo stripslashes(get_option('my_ad_content_bottom2')); ?>
+<div class="ad_section">
+    <p class="ad_title">広告</p>
+    <div class="clear">
+        <div class="ad">
+            <?php echo stripslashes(get_option('my_ad_content_bottom1')); ?>
+        </div>
+        <div class="ad">
+            <?php echo stripslashes(get_option('my_ad_content_bottom2')); ?>
+        </div>
     </div>
 </div>
 			<?php endif; // is_single() ?>
