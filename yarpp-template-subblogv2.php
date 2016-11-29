63668b0d44f82347f58119c0fbc17d894946e0ed
fukata <tatsuya.fukata@gmail.com>
2016-11-29 13:51:19 +0700

    update template

--- yarpp-template-subblogv2.php
+++ yarpp-template-subblogv2.php
@@ -4,19 +4,21 @@
  * Description: Requires a theme which supports post thumbnails
  * Author: fukata (Tatsuya Fukata)
  * */ ?>
-<div class="yarpp-related">
-  <h2>関連記事</h2>
-  <div class="yarpp-thumbnails-horizontal">
-    <?php if (have_posts()):?>
-      <?php while (have_posts()) : the_post(); ?>
-        <a class="yarpp-thumbnail" href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">
-          <span class="yarpp-thumbnail-default"><img src="<?php the_post_thumbnail(); ?>"></span><span class="yarpp-thumbnail-title"><?php the_title_attribute(); ?></span>
-        </a>
-      <?php endwhile; ?>
-    </ol>
-  </div>
-
-  <?php else: ?>
-  <p>まだ関連記事がありません。</p>
-  <?php endif; ?>
+<h2>関連記事</h2>
+<div class="yarpp-thumbnails-horizontal">
+<?php if (have_posts()):?>
+<?php while (have_posts()) : the_post(); ?>
+<a class="yarpp-thumbnail" href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">
+  <?php if ( FlickrPress::isExtractThumbnailByPostID( get_the_ID() ) ) { ?>
+  <span class="yarpp-thumbnail-default"><?php the_post_thumbnail('q'); ?></span><span class="yarpp-thumbnail-title"><?php the_title(); ?></span>
+  <?php } else { ?>
+  <span class="yarpp-thumbnail-default"><?php the_post_thumbnail('thumbnail'); ?></span><span class="yarpp-thumbnail-title"><?php the_title(); ?></span>
+  <?php } ?>
+</a>
+<?php endwhile; ?>
+</ol>
 </div>
+
+<?php else: ?>
+<p>まだ関連記事がありません。</p>
+<?php endif; ?>
