b8bb571c286fc401bff35b42692390ff6c1d48aa
fukata <tatsuya.fukata@gmail.com>
2016-11-29 15:09:38 +0700

    move ad

--- yarpp-template-subblogv2.php
+++ yarpp-template-subblogv2.php
@@ -21,12 +21,10 @@ if ( empty($thumbnails_default) ) {
 
 $dimensions = $this->thumbnail_dimensions();
 
-$output .= <<<EOT
-<div class="ad_section">
-  <div class="ad_title">広告</div>
-  <div class="ad_body"><?php echo stripslashes(get_option('my_ad_content_bottom2')); ?></div>
-</div>
-EOT;
+$output .= '<div class="ad_section">' .
+           '<div class="ad_title">広告</div>' .
+           '<div class="ad_body"> ' . stripslashes(get_option('my_ad_content_bottom2')) . '</div>' . 
+           '</div>';
 
 $output .= '<h2>関連記事</h2>' . "\n";
 
