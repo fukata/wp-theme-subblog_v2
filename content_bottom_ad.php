85ac5e0239cffc66ed1ec8ad323a3971f34b9514
fukata <tatsuya.fukata@gmail.com>
2017-09-22 16:01:25 +0700

    remove ad title

--- content_bottom_ad.php
+++ content_bottom_ad.php
@@ -1,7 +1,5 @@
             <?php if ( is_single() ) : ?>
 <div class="ad_section">
-  <div class="ad_title">広告</div>
   <div class="ad_body"><?php echo stripslashes(get_option('my_ad_content_bottom3')); ?></div>
 </div>
 			<?php endif; // is_single() ?>
-
