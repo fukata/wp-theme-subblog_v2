ebeccf98b12ab11028bd9ca346269dbdcb6a7f4b
fukata <tatsuya.fukata@gmail.com>
2017-09-23 01:08:28 +0700

    add title

--- content_bottom_ad.php
+++ content_bottom_ad.php
@@ -1,5 +1,6 @@
             <?php if ( is_single() ) : ?>
 <div class="ad_section">
+  <h2>関連記事</h2>
   <div class="ad_body"><?php echo stripslashes(get_option('my_ad_content_bottom3')); ?></div>
 </div>
 			<?php endif; // is_single() ?>
