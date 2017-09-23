a48af8fa9d1da317bc7a0f1e1ad59417c4d9a9a8
fukata <tatsuya.fukata@gmail.com>
2017-09-23 12:07:35 +0700

    update style

--- category-link.php
+++ category-link.php
@@ -1,11 +1,11 @@
 <?php $category = get_the_category()[0]; ?>
 <?php if ($category->slug == "eating") { ?>
-<ul class="category-child-links">
+<ul class="category-child-links clear">
   <li><a href="/archives/category/<?php echo $category->slug ?>?tag=日本">日本</a></li>
   <li><a href="/archives/category/<?php echo $category->slug ?>?tag=タイ">タイ</a></li>
 </ul>
 <?php } else if ($category->slug == "travel") { ?>
-<ul class="category-child-links">
+<ul class="category-child-links clear">
   <li><a href="/archives/category/<?php echo $category->slug ?>?tag=日本">日本</a></li>
   <li><a href="/archives/category/<?php echo $category->slug ?>?tag=台湾">台湾</a></li>
   <li><a href="/archives/category/<?php echo $category->slug ?>?tag=タイ">タイ</a></li>
