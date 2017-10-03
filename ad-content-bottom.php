0e9d032d7f0587404098536f356ef3170c295af7
fukata <tatsuya.fukata@gmail.com>
2017-10-03 18:31:20 +0700

    add ad

--- ad-content-bottom.php
+++ ad-content-bottom.php
@@ -1,4 +1,5 @@
 <?php if ( is_single() ) : ?>
+  <?php echo stripslashes(get_option('my_ad_content_bottom2')); ?>
   <h2>関連記事</h2>
   <?php echo stripslashes(get_option('my_ad_content_bottom3')); ?>
 <?php endif; // is_single() ?>
